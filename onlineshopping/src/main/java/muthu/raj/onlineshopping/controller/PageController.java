f012f53d491e071f048b544c1fe2d7b4a52c40a9
Muthuraj <muthuraj5.official@gmail.com>
2018-08-06 22:38:13 +0530

    refs #master ui commit

--- onlineshopping/src/main/java/muthu/raj/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/muthu/raj/onlineshopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package muthu.raj.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,5 +15,5 @@ public class PageController {
 		mv.addObject("greeting","welcome to spring web mvc");
 		return mv;
 	}
-
+	
 }
